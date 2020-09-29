71f146abef9814c80a5c0c84d34100307498c43f
Lyle Rallos <61070788+dubz-skrrrt@users.noreply.github.com>
2020-09-29 21:27:42 +0800

    PlayerController

---
+++ Drop Out/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float strafeSpeed;
+    public float jumpForce;
+
+    public Rigidbody hips;
+    public bool isGrounded;
+
+    void start()
+    {
+        hips = GetComponent<Rigidbody>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            hips.AddForce(hips.transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            hips.AddForce(-hips.transform.right * strafeSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            hips.AddForce(-hips.transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            hips.AddForce(hips.transform.right * strafeSpeed);
+        }
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (isGrounded)
+            {
+                hips.AddForce(new Vector3(0, jumpForce, 0));
+                isGrounded = false;
+            }
+        }
+    }
+}
