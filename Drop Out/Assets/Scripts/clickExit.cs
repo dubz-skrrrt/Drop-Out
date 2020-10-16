9156e4a9a0d825904edb8d545f1a5490d7fa1f3a
Minnic27 <jdCabig@mcm.edu.ph>
2020-10-16 21:16:18 +0800

    import main-menu

---
+++ Drop Out/Assets/Scripts/clickExit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickExit : MonoBehaviour
+{
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
