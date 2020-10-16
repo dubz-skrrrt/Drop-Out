86567255f843bf81c6ab3ee889150828b1aca0a9
Minnic27 <jdCabig@mcm.edu.ph>
2020-10-16 21:26:08 +0800

    finish line

--- Drop Out/Assets/Scripts/FinishLine.cs
+++ Drop Out/Assets/Scripts/FinishLine.cs
@@ -1,24 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     //private bool complete = false;
     private int qualified = 0;
-    private int maxQualification = 10;
+    private int maxQualification = 1;
 
     void OnTriggerEnter(Collider col)
     {
         // if player or enemy collides with finish line
-        if ((col.gameObject.tag == "Player") || (col.gameObject.tag == "Enemy"))
+        if (col.gameObject.tag == "Player")
         {
             qualified++;
             if (qualified == maxQualification)
             {
                 //complete = true;
                 Debug.Log("WIPEOUT!");
-                Time.timeScale = 0;
+                SceneManager.LoadScene("UI2"); // loads game
             }
             
         }
