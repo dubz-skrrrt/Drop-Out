d66430bb701600c9639e29c726dcd4e7d57a11b2
Minnic27 <jdCabig@mcm.edu.ph>
2020-10-16 20:44:52 +0800

    rotation arm

---
+++ Drop Out/Assets/Scripts/RotatingArm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingArm : MonoBehaviour
+{
+    private float speed = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+}
