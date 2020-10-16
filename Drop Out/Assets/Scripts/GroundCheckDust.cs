9058e0642ae16c623c7b117edb99669c64528be9
Lyle Rallos <61070788+dubz-skrrrt@users.noreply.github.com>
2020-10-16 18:33:22 +0800

    particle effect dustCloud

---
+++ Drop Out/Assets/Scripts/GroundCheckDust.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckDust : MonoBehaviour
+{
+    public GameObject dustCloud;
+
+   void OnTriggerEnter(Collider other)
+   {
+       if (other.gameObject.tag.Equals("ObstacleCourse")){
+           Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+       }
+   }
+}
