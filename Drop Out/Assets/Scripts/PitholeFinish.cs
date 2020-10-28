44ab2ff632a3f9b12f315303ec5b38e6931e2043
Minnic27 <jdCabig@mcm.edu.ph>
2020-10-28 16:16:11 +0800

    Pithole changes

---
+++ Drop Out/Assets/Scripts/PitholeFinish.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PitholeFinish : MonoBehaviour
+{
+    private int remaining = 2;
+    private int maxQualification = 1;
+    
+    void OnTriggerEnter(Collider col)
+    {
+        // if player or enemy collides with pithole
+        if (col.gameObject.tag == "Player")
+        {
+            remaining--;
+            if (remaining == maxQualification)
+            {
+                SoundManager.PlaySound("Yipee");
+                Debug.Log("WIPEOUT!");
+                Cursor.lockState = CursorLockMode.None;
+                StartCoroutine(DelayNextLevel());
+            }
+            
+        }
+    }
+
+    IEnumerator DelayNextLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("MainMenu"); // loads mainmenu [next level if build is completed]
+    }
+}
