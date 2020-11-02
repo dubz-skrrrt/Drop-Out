caae9e566a4a9593fdedfcdcae23ecc3215fb69d
Ankoopa <dmdasalla@mcm.edu.ph>
2020-11-02 20:54:46 +0800

    added ai script

---
+++ Drop Out/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
