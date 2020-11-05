e74ec3757d523efed0d50f1147152767fda41caf
Minnic27 <jdCabig@mcm.edu.ph>
2020-11-05 21:06:07 +0800

    MortarEnemy programming

---
+++ Drop Out/Assets/Scripts/MortarFireProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarFireProjectile : MonoBehaviour
+{
+    public Rigidbody projectilePrefab;
+    public Transform cannonTip;
+    public float delaySeconds;
+
+    void Start()
+    {
+        StartCoroutine("waitForSpawn");
+    }
+
+    IEnumerator waitForSpawn()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds (delaySeconds);
+
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectilePrefab, cannonTip.position, cannonTip.rotation) as Rigidbody;
+            projectileInstance.AddForce(cannonTip.forward * 1000);
+            
+        }
+    }
+}
---
+++ Drop Out/Assets/Scripts/MortarProjectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarProjectile : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "ProjectileDeath")
+        {
+            Destroy(this.gameObject, 5);
+        }
+    }
+}
