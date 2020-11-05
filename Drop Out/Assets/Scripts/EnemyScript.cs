528dbdc4879ce6a7c3ebacce96fe425ba869082b
Ankoopa <dmdasalla@mcm.edu.ph>
2020-11-05 21:29:38 +0800

    animation partially working, slightly better pathfinding

--- Drop Out/Assets/Scripts/EnemyScript.cs
+++ Drop Out/Assets/Scripts/EnemyScript.cs
@@ -6,17 +6,31 @@ using UnityEngine.AI;
 public class EnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float jumpForce;
     public Transform goal;
+    private Rigidbody rb;
+    private Animator anim;
+    private NavMeshAgent agent;
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+
+        anim = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("Running", true);
+        if (agent.isOnOffMeshLink){
+            Vector3 rbVel = rb.velocity;
+            rbVel.y = jumpForce;
+            rb.velocity = rbVel;
+            anim.SetTrigger("Jump");
+        }
     }
 }
