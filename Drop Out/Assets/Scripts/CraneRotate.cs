cbe817067d00c985ac31d166413224674c050eb6
Lyle Rallos <61070788+dubz-skrrrt@users.noreply.github.com>
2020-11-06 22:37:01 +0800

    Crane Rotate

---
+++ Drop Out/Assets/Scripts/CraneRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+}
